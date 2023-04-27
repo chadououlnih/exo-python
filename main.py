c289114228cc792ac03899e0747a670cc4f8271c
ShadouNih <chadououlnih@gmail.com>
2023-04-27 19:42:09 +0000

    last version

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("salam");
+print("salam")
+myName = "Ahmed"
+print("my name is "+ myName)
+myAge = 60
+#print("I am "); print(myAge)
+a = input("Whats your name visitor: ")
+print("hello "+a)
+b = int(input("What's your age: "))
+if b < myAge:
+    print("you are younger than me, still have to grow", myAge - b, " years")
+elif b > myAge:
+    print("You are older than me, I still have to grow"+ b - myAge+ " years")
+guess = int(input("Can you guess my age: "))
+while myAge!= guess:
+ if guess < myAge:
+  print("I am sorry")
+  guess = int(input("try Again: "))
+ elif guess > myAge:
+  print("I am sorry")
+  guess = int(input("try Again: "))
+ else:
+  break
+print("you guessed it right!!")
